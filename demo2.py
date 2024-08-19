5a9328793f11ab6d838f26e516b53ca6e2d1ada7
Venkata Mahendra Reddy Karnati <121597059+karnativmr@users.noreply.github.com>
2024-08-18 21:30:41 -0700

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -3,5 +3,5 @@ numbers = [1, 2, 3, 4, 5]
 
 # Loop through the list and print the square of each number
 for number in numbers:
-    square = number ** 2
+    square = number ** 3
     print(f"The square of {number} is {square}")
